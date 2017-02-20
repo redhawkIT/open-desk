9fc62b425a4c49cfe7c2bdc13b8cdae5837133f5
redhawkIT <khodkodmitry10011983@hotmail.com>
2017-02-19 22:36:20 -0800

    Added chat text area

--- app/components/NavItems.js
+++ app/components/NavItems.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { hashHistory } from 'react-router' //  For pushing links w/o Router
 
+import ListItem from 'react-md/lib/Lists/ListItem'
+import Avatar from 'react-md/lib/Avatars'
 import FontIcon from 'react-md/lib/FontIcons'
 
+import TextField from 'react-md/lib/TextFields'
+
 //  Array is raw due to the unique nature of each element
-const NavItems = [{
-  primaryText: 'Dashboard',
-  leftIcon: <FontIcon>home</FontIcon>,
-  onClick: () => (hashHistory.push('/'))
-}, {divider: true}, {
-  primaryText: 'Contact', subheader: true
-}, {divider: true}, {
-  primaryText: 'Admin@company.org',
-  secondaryText: 'Web Administrator',
-  leftIcon: <a href='mailto:Admin@company.org' alt=''><FontIcon>mail</FontIcon></a>,
-  tileClassName: 'selectable'
-}, {divider: true}, <div>TEST</div>]
+const NavItems = [
+  <ListItem
+    leftAvatar={<Avatar src='http://derfunknoid.com/logo.png' role='presentation' />}
+    // rightIcon={<FontIcon>star</FontIcon>}
+    primaryText='Barren Skew'
+    secondaryText={'Senior Graphics Coordinator\nGraphics Department'}
+    threeLines
+  />, {divider: true},
+  <div>
+    Chat Messages Here
+  </div>, {divider: true},
+  <TextField
+    id='floatingMultiline'
+    label='Company Chat'
+    // placeholder="Hello World"
+    lineDirection='right'
+    rows={4}
+    className='md-cell md-cell--bottom'
+  />
+]
 
 export default NavItems
