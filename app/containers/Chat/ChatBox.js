c0868a52983c79e78782cd440b7747f65bcb2f82
redhawkIT <khodkodmitry10011983@hotmail.com>
2017-02-23 14:46:11 -0800

    Got the template, time for crud.

--- app/containers/Chat/ChatBox.js
+++ app/containers/Chat/ChatBox.js
@@ -6,12 +6,19 @@ import Paper from 'react-md/lib/Papers'
 //  Array is raw due to the unique nature of each element
 const ChatBox = () => (
   <div className='chat-box'>
-    <div className='chat-message'>
+    <div className='message'>
       <Avatar src='http://derfunknoid.com/logo.png' iconSized role='presentation' />
-      <Paper style={{borderRadius: '0 8px 8px 8px'}}>
+      <div className='content receiver'>
         <span className='md-text--secondary'>Darren | 12:41pm</span><br />
         Hello World I'm going to talk about myself and my packages nyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenye
-      </Paper>
+      </div>
+    </div>
+    <div className='message'>
+      <div className='content sender'>
+        <span className='md-text--secondary'>Darren | 12:41pm</span><br />
+        Hello World I'm going to talk about myself and my packages nyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenye
+      </div>
+      <Avatar src='http://derfunknoid.com/logo.png' iconSized role='presentation' />
     </div>
     <div className='chat-message'>
 
