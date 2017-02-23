d4b3c3b63d9ed8c0e4b8542514b177e07af778c9
redhawkIT <khodkodmitry10011983@hotmail.com>
2017-02-23 15:03:41 -0800

    Abusing rtl to get the right alignment

--- app/containers/Chat/ChatBox.js
+++ app/containers/Chat/ChatBox.js
@@ -6,11 +6,27 @@ import Paper from 'react-md/lib/Papers'
 //  Array is raw due to the unique nature of each element
 const ChatBox = () => (
   <div className='chat-box'>
+
     <div className='message'>
       <Avatar src='http://derfunknoid.com/logo.png' iconSized role='presentation' />
       <div className='content receiver'>
         <span className='md-text--secondary'>Darren | 12:41pm</span><br />
         Hello World I'm going to talk about myself and my packages nyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenye
+      </div>
+    </div>
+    <div className='message' style={{direction: 'rtl'}}>
+      <Avatar src='http://derfunknoid.com/logo.png' iconSized role='presentation' />
+      <div className='content sender'>
+        <span className='md-text--secondary'>Darren | 12:41pm</span><br />
+        Hello World
+      </div>
+    </div>
+
+    <div className='message'>
+      <Avatar src='http://derfunknoid.com/logo.png' iconSized role='presentation' />
+      <div className='content receiver'>
+        <span className='md-text--secondary'>Darren | 12:41pm</span><br />
+        Hello World
       </div>
     </div>
     <div className='message'>
@@ -20,30 +36,7 @@ const ChatBox = () => (
       </div>
       <Avatar src='http://derfunknoid.com/logo.png' iconSized role='presentation' />
     </div>
-    <div className='chat-message'>
 
-      <Paper style={{borderRadius: '8px 0 8px 8px'}}>
-        <span className='md-text--secondary'>Darren | 12:41pm</span><br />
-        Hello World I'm going to talk about myself and my packages nyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenye
-      </Paper>
-      <Avatar src='http://derfunknoid.com/logo.png' iconSized role='presentation' />
-    </div>
-
-    <div className='chat-message'>
-      <Avatar src='http://derfunknoid.com/logo.png' iconSized role='presentation' />
-      <Paper style={{borderRadius: '0 8px 8px 8px'}}>
-        <span className='md-text--secondary'>Darren | 12:41pm</span><br />
-        Hello World I'm going to talk about myself and my packages nyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenye
-      </Paper>
-    </div>
-    <div className='chat-message'>
-
-      <Paper style={{borderRadius: '8px 0 8px 8px'}}>
-        <span className='md-text--secondary'>Darren | 12:41pm</span><br />
-        Hello World I'm going to talk about myself and my packages nyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenyenye
-      </Paper>
-      <Avatar src='http://derfunknoid.com/logo.png' iconSized role='presentation' />
-    </div>
   </div>
 )
 
