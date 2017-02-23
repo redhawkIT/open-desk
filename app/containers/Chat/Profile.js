be29479c76ce332234fb500a4cf28db15509b138
redhawkIT <khodkodmitry10011983@hotmail.com>
2017-02-22 16:07:40 -0800

    Added login element.

--- app/containers/Chat/Profile.js
+++ app/containers/Chat/Profile.js
@@ -3,19 +3,28 @@ import React from 'react'
 import ListItem from 'react-md/lib/Lists/ListItem'
 import Avatar from 'react-md/lib/Avatars'
 
-//photoURL | displayName | email
+// photoURL | displayName | email
 // threeLines
-
-const Profile = ({ user = false, authenticate}) => (
+const Profile = ({ user = false, authenticate }) => (
   <div>
-  {user ?
-    <ListItem
-      leftAvatar={<Avatar src={user.photoURL} role='presentation' />}
+    {user ? <ListItem
+      leftAvatar={
+        <Avatar src={user.photoURL} role='presentation' />
+      }
+      threeLines
       primaryText={user.displayName}
-      secondaryText={user.email}
+      secondaryText={
+        <span>{`${user.email}\n`}<em>Click to logout</em></span>
+      }
     />
-  : <div onClick={authenticate}>Please Log in</div>}
-</div>
+    : <ListItem onClick={authenticate}
+      threeLines
+      primaryText={<b>Click to Login</b>}
+      secondaryText={<em>Authentication by Google</em>}
+    />
+
+    }
+  </div>
 )
 
 export default Profile
